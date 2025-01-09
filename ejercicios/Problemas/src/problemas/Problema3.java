28f0c9be417a35c6a3f1ae7d74937e601500b6ee
Eduardo Peñarreta <espenarreta@utpl.edu.ec>
2025-01-09 12:02:52 -0500

    Eduardo Peñarreta

--- ejercicios/Problemas/src/problemas/Problema3.java
+++ ejercicios/Problemas/src/problemas/Problema3.java
@@ -5,6 +5,8 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -16,6 +18,54 @@ public class Problema3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+
+        int nota1;
+        int nota2;
+        int nota3;
+        int nota4;
+        String mensaje = "";
+
+        System.out.println("Ingrese la nota 1");
+        nota1 = entrada.nextInt();
+        System.out.println("Ingrese la nota 2");
+        nota2 = entrada.nextInt();
+        System.out.println("Ingrese la nota 3");
+        nota3 = entrada.nextInt();
+        System.out.println("Ingrese la nota 4");
+        nota4 = entrada.nextInt();
+
+        mensaje = obtenerPromedio(nota1, nota2, nota3, nota4);
+        mensaje = String.format("El promedio de las notas" + nota1 + ", "
+                + nota2 + ", " + nota3 + ", " + nota2 + " es " + mensaje);
+        System.out.printf("%s\n", mensaje);
     }
-    
+
+    public static String obtenerPromedio(double a, double b, double c, double d) {
+        
+        double promedio;
+        double suma;
+        String mensaje = "";
+
+        suma = a + b + c + d;
+        promedio = suma / 4;
+
+        if (promedio >= 0 && promedio
+                <= 5) {
+            mensaje = "Regular";
+        } else {
+            if (promedio >= 5.1 && promedio <= 8) {
+                mensaje = "Bueno";
+            } else {
+                if (promedio >= 8.1 && promedio <= 9) {
+                    mensaje = "Muy bueno";
+                } else {
+                    if (promedio >= 9.1 && promedio <= 10) {
+                        mensaje = "Sobresaliente";
+                    }
+                }
+            }
+        }
+        return mensaje;
+    }
 }
