b6526d4a0ebd873dafd9cfb4c3b57d4932ee297c
Eduardo Peñarreta <espenarreta@utpl.edu.ec>
2025-01-09 11:45:13 -0500

    Eduardo Peñarreta

--- ejercicios/Problemas/src/problemas/Problema1.java
+++ ejercicios/Problemas/src/problemas/Problema1.java
@@ -20,12 +20,13 @@ public class Problema1 {
     }
 
     public static void imprimirDatos(int[][] datos) {
-        for (int i = 0; i <= datos.length; i++){
-            for (int j = 0; j <= i; j++)
-            if(datos[i][j] % 2 == 0){
-                System.out.printf("%d\n", datos[i][j]);
+        for (int i = 0; i <= datos.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (datos[i][j] % 2 == 0) {
+                    System.out.printf("%d\n", datos[i][j]);
+                }
             }
         }
     }
-    
+
 }
--- ejercicios/Problemas/src/problemas/Problema2.java
+++ ejercicios/Problemas/src/problemas/Problema2.java
@@ -5,6 +5,8 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -16,6 +18,69 @@ public class Problema2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+
+        int area;
+
+        System.out.println("Ingrese el número de la figura (1 para cuadrado),"
+                + " (2 para triángulo), (3 para rectangulo)");
+        area = entrada.nextInt();
+
+        if (area == 1) {
+            obtenerAreaCuadrado();
+        } else {
+            if (area == 2) {
+                obtenerAreaTriangulo();
+            } else {
+                if (area == 3) {
+                    obtenerAreaRectangulo();
+                }
+            }
+        }
+
     }
-    
+
+    public static void obtenerAreaCuadrado() {
+        Scanner entrada = new Scanner(System.in);
+        int area;
+        int lado;
+        System.out.println("Ingrese el valor del lado del cuadrado");
+        lado = entrada.nextInt();
+        
+        area = lado * lado;
+        
+        System.out.printf("El área del cuadrado es: %d\n", area);
+    }
+
+    public static void obtenerAreaTriangulo() {
+        Scanner entrada = new Scanner(System.in);
+        int area;
+        int base;
+        int altura;
+        System.out.println("Ingrese el valor de la base");
+        base = entrada.nextInt();
+        System.out.println("Ingrese el valor de la altura");
+        altura = entrada.nextInt();
+        
+        area = (base * altura)/2;
+        
+        System.out.printf("El área del triángulo es: %d\n", area);
+    }
+
+    public static void obtenerAreaRectangulo() {
+        Scanner entrada = new Scanner(System.in);
+        int area;
+        int base;
+        int altura;
+        
+        System.out.println("Ingrese el valor de la base");
+        base = entrada.nextInt();
+        System.out.println("Ingrese el valor de la altura");
+        altura = entrada.nextInt();
+        
+        area = base * altura;
+        
+        System.out.printf("El área del rectangulo es: %d\n", area);
+    }
+
 }
