1af33818bd4b8b0adeeb60094d4c6a8ef17e7c2d
Eduardo Peñarreta <espenarreta@utpl.edu.ec>
2025-01-09 17:08:38 -0500

    Problemas resueltos

--- ejercicios/Problemas/src/problemas/Problema6.java
+++ ejercicios/Problemas/src/problemas/Problema6.java
@@ -10,5 +10,25 @@ package problemas;
  * @author reroes
  */
 public class Problema6 {
+    public static void main(String[] args) {
+        
+        int[] informacion = {1, 2, 3, 10, 20, 30, 100, 200, 300};
+        
+        int media = mediaAritmetica(informacion);
+        
+        System.out.printf("La media del arreglo es: %d\n", media);
+        
+    }
     
+    public static int mediaAritmetica(int [] media) {
+        
+        int suma = 0;
+        int promedio;
+        
+        for (int i = 0; i <= media.length; i++) {
+            suma = suma + media[i];
+        }
+        // promedio = suma/media.length;
+        return suma;
+    }
 }
